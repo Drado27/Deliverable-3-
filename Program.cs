5ad01c47ca4a26300a86c0cd90d2689ec7d45894
Drado27 <122625400+Drado27@users.noreply.github.com>
2023-01-29 12:13:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ Author: Domenick Rado
+Date:1/29/2023
+Description: C# console application for iterative statements
+*/
+
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Deliverable_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter an integer number between 1 and 100: ");
+            try
+            {
+
+
+                int input = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Specify the series tye: even or odd ");
+                string series = Console.ReadLine();
+
+                if (series == "odd")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+                    for (int i = 1; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "even")
+                {
+                    Console.WriteLine("You have selected " + series + " series. The numbers between 0 and " + input + " are: ");
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter an integer number between 1 and 100: ");
+            }
+        }
+    } 
+}
